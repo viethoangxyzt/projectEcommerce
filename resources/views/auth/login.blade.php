5e56a338be4484962d2424be73cf182e9c3f32ee
kiaisoft.hoang.cao <kiaisoft.hoang.cao@gmail.com>
2023-07-17 15:45:16 +0700

    update font

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,62 +2,62 @@
 @section('content-client')
 <div class="container_fullwidth content-page">
   <div class="container">
-    <div class="col-md-12 container-page">
+    <div class="col-md-2"></div>
+    <div class="col-md-8 container-page ">
       <div class="checkout-page">
         <ol class="checkout-steps">
           <li class="steps active">
-            <h4 class="title-steps text-center">
-              Đăng Nhập
-            </h4>
+            <h4 class="step-title text-center" style="font-weight: bold; color: blue; font-size: 24px;">Đăng Nhập</h4>
             <div class="step-description">
               <div class="row">
-                <div class="col-md-2 col-sm-2"></div>
-                <div class="col-md-8 col-sm-8 ">
+                <div class="col-md-12 col-sm-12" style="margin-top: 30px">
                   <div class="run-customer">
                     <form action="{{ route('user.login') }}" method="POST" id="login-form__js">
-                      @csrf
-                      <div class="form-group">
-                        <label for="exampleInputEmail1">Email</label>
-                        <input type="text" class="form-control" value="{{ old('email') }}" id="email" name="email" placeholder="Nhập email">
-                        @if ($errors->get('email'))
-                        <span id="email-error" class="error invalid-feedback" style="display: block">
+                      @csrf          
+                      <div class="form-row ">
+                        <label class="lebel-abs">
+                          Email
+                          <strong class="red">
+                            *
+                          </strong>
+                        </label>
+                        <input type="text" class="input namefild" value="{{ old('email') }}" id="email" name="email">
+                        <span id="email-error" class="error invalid-feedback" style="display: block; color: red;">
                           {{ implode(", ",$errors->get('email')) }}
                         </span>
-                        @endif
                       </div>
-                      <div class="form-group">
-                        <label for="exampleInputPassword1">Mật Khẩu</label>
-                        <input type="password" class="form-control" id="password" name="password" placeholder="Nhập mật khẩu">
-                        @if ($errors->get('password'))
-                        <span id="password-error" class="error invalid-feedback" style="display: block">
+                      <div class="form-row" style="margin-top:40px;">
+                        <label class="lebel-abs">
+                          Mật khẩu
+                          <strong class="red">
+                            *
+                          </strong>
+                        </label>
+                        <input type="password" class="input namefild" id="password" name="password">
+                        <span id="password-error" class="error invalid-feedback" style="display: block; color: red;">
                           {{ implode(", ",$errors->get('password')) }}
                         </span>
-                        @endif
                       </div>
-                      <div class="text-center">
-                        <button>
-                          Đăng Nhập
-                        </button>
+                      <div class="text-center" style="margin-top: 30px">
+                        <button style="height: 45px;">Đăng Nhập</button>
                       </div>
                       <div class="content-footer text-right" style="margin-top: 20px;">
-                        <a href="{{ route('user.forgot_password_create') }}">
-                          Quên mật khẩu |
-                        </a>
-                        <a href="{{ route('user.register') }}">
-                          Đăng kí tài khoản
-                        </a>
+                        <a href="{{ route('user.forgot_password_create') }}">Quên mật khẩu</a> |
+                        <a href="{{ route('user.register') }}">Đăng kí tài khoản</a>
                       </div>
                     </form>
                     <a href="{{ route('login.google') }}" class="btn btn-danger btn-block mt-3">Login with Google</a>
                   </div>
                 </div>
-                <div class="col-md-2 col-sm-2"></div>
               </div>
             </div>
+          </li>
         </ol>
       </div>
     </div>
+    <div class="col-md-2"></div>
   </div>
 </div>
 @vite(['resources/common/js/login.js'])
+@vite(['resources/common/css/validation.css'])
 @endsection
